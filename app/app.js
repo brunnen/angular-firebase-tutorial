457f9c1459149aa1d1befe49eb41193fb8d09b2c
Bruno Gomes <brunompg@gmail.com>
2015-09-17 11:28:21 +0100

    Added new post page

--- app/app.js
+++ app/app.js
@@ -2,11 +2,12 @@
  
 angular.module('myApp', [
     'ngRoute',
-    'myApp.home',           // Newly added home module
-    'myApp.register',  // Newly added register route
-    'myApp.welcome'   // Newly added module
-]).
-config(['$routeProvider', function($routeProvider) {
+    'myApp.home',
+    'myApp.register',
+    'myApp.welcome',
+    'myApp.addPost'     // Newly added module
+])
+.config(['$routeProvider', function($routeProvider) {
     // Set defualt view of our app to home
      
     $routeProvider.otherwise({
